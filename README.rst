154773d17735f48e15a455192ca4b084a4d85492
Zoran Simic <zsimic@netflix.com>
2018-06-22 15:51:15 -0700

    Initial draft

---
+++ README.rst
@@ -0,0 +1,4 @@
+YAML parser
+===========
+
+Simple YAML parser, in pure python, without any dependencies.
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(
+    name='yaml',
+    setup_requires='setupmeta',
+    versioning='post',
+    author='Zoran Simic zoran@simicweb.com',
+    url='https://github.com/zsimic',
+)
