db2ac0de73e262c87684e5fd1cf1ff8e89ba246d
Zoran Simic <zsimic@netflix.com>
2020-10-30 17:09:03 -0700

    Synced setup with the rest

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
 
-setup(
-    name="zyaml",
-    setup_requires="setupmeta",
-    versioning="dev",
-    author="Zoran Simic zoran@simicweb.com",
-    url="https://github.com/zsimic/sandbox",
-)
+if __name__ == "__main__":
+    setup(
+        name="zyaml",
+        setup_requires="setupmeta",
+        versioning="dev",
+        author="Zoran Simic zoran@simicweb.com",
+        url="https://github.com/zsimic/sandbox",
+    )
